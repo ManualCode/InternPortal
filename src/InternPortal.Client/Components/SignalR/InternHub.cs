b54d73a4ed1ac3489cefaf64ddebb020e5aa01cb
ManualCode <kiriltimoxov@gmail.com>
2025-04-30 17:14:08 +0500

    -Убрал хардкод в строках подключения

---
+++ src/InternPortal.Client/Components/SignalR/InternHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace InternPortal.Client.Components.SignalR
+{
+    public class InternHub
+    {
+        private readonly HubConnection Hub;
+
+        public InternHub(IConfiguration configuration)
+        {
+            var apiBaseUrl = configuration["ApiBaseUrl"];
+            var hubPath = configuration["HubSignalRPath"];
+
+            Hub = new HubConnectionBuilder()
+                .WithUrl($"{apiBaseUrl}{hubPath}")
+                .WithAutomaticReconnect()
+                .Build();
+
+            Hub.StartAsync();
+        }
+
+        public async void Sender(string methodName, object data)
+            => await Hub.InvokeAsync(methodName, data);
+
+        public async void Receiver<T>(string methodName, Action<T> action)
+        {
+            Hub.On<T>(methodName, action);
+        }
+    }
+}
